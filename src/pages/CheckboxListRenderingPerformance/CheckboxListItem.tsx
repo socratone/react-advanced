876b3f23a7c437dd240dde985e70b1ef5da4953f
Kiwon Kim <kiwon@elicer.com>
2022-12-11 23:01:56 +0900

    feat: react window 적용

--- src/pages/CheckboxListRenderingPerformance/CheckboxListItem.tsx
+++ src/pages/CheckboxListRenderingPerformance/CheckboxListItem.tsx
@@ -8,15 +8,17 @@ interface CheckboxListItemProps {
     checked: boolean
   ) => void;
   label: string;
+  style: React.CSSProperties;
 }
 
 const CheckboxListItem: React.FC<CheckboxListItemProps> = ({
   checked,
   onChange,
   label,
+  style,
 }) => {
   return (
-    <Container>
+    <Container style={style}>
       <Checkbox checked={checked} onChange={onChange} />
       <Typography>{label}</Typography>
     </Container>
--- src/pages/CheckboxListRenderingPerformance/index.tsx
+++ src/pages/CheckboxListRenderingPerformance/index.tsx
@@ -1,15 +1,25 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
 import CheckboxListItem from './CheckboxListItem';
+import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
 
+type Item = {
+  id: string;
+  label: string;
+  checked: boolean;
+};
+
+/**
+ * react-window를 이용해 화면에 보이는 부분만 렌더링하여 과도한 리렌더링으로 인한 퍼포먼스 문제 해결
+ */
 const CheckboxListRenderingPerformancePage = () => {
-  const [checkboxList, setCheckboxList] = useState(() => {
+  const [checkboxList, setCheckboxList] = useState<Item[]>(() => {
     const list = [];
     for (let i = 1; i <= 1000; i++) {
       list.push({
         id: String(i),
-        checked: false,
         label: `${i}번째 아이템`,
+        checked: false,
       });
     }
     return list;
@@ -45,16 +55,35 @@ const CheckboxListRenderingPerformancePage = () => {
     <Container>
       <Button onClick={toggleAll}>전체 선택</Button>
       <ScrollableBox>
-        {checkboxList.map((item) => (
-          <CheckboxListItem
-            key={item.id}
-            label={item.label}
-            checked={item.checked}
-            onChange={(_, checked) => handleChange(item.id, checked)}
-          />
-        ))}
+        <List
+          height={400} // list height
+          itemCount={checkboxList.length}
+          itemSize={40} // item height
+          width={400}
+          itemData={{ checkboxList, onChange: handleChange }} // custom props
+        >
+          {Row}
+        </List>
       </ScrollableBox>
     </Container>
+  );
+};
+
+const Row: React.FC<
+  ListChildComponentProps<{
+    checkboxList: Item[];
+    onChange: (id: string, checked: boolean) => void;
+  }>
+> = ({ index, style, data }) => {
+  const item = data.checkboxList[index];
+
+  return (
+    <CheckboxListItem
+      style={style}
+      label={item.label}
+      checked={item.checked}
+      onChange={(_, checked) => data.onChange(item.id, checked)}
+    />
   );
 };
 
@@ -78,9 +107,10 @@ const Button = styled.button`
 
 const ScrollableBox = styled.div`
   border: 1px solid black;
-  height: 500px;
-  width: 500px;
+  height: 400px;
+  width: 400px;
   overflow-y: auto;
+  box-sizing: border-box;
 `;
 
 export default CheckboxListRenderingPerformancePage;
